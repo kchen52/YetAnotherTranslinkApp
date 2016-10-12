f5ba0386b209dfff8f886fc863b8c0c03df7d0cb
Kevin Chen <k.chen52@gmail.com>
2016-10-12 16:36:08 -0700

    Removed unneeded imports

--- app/src/main/java/com/kchen52/yetanothertranslinkapp/BusHandler.java
+++ app/src/main/java/com/kchen52/yetanothertranslinkapp/BusHandler.java
@@ -3,7 +3,6 @@ package com.kchen52.yetanothertranslinkapp;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
-import android.util.Log;
 
 import java.util.Date;
 import java.util.LinkedList;
--- app/src/main/java/com/kchen52/yetanothertranslinkapp/RequestHandler.java
+++ app/src/main/java/com/kchen52/yetanothertranslinkapp/RequestHandler.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.telephony.SmsManager;
-import android.util.Log;
 
 public class RequestHandler {
     private SharedPreferences sharedPref;
