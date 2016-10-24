31042fc97f9938433cf5481943e7476f2793871d
Kevin Chen <k.chen52@gmail.com>
2016-10-23 23:55:10 -0700

    Added catch clauses for invalid input for init()

--- app/src/main/java/com/kchen52/yetanothertranslinkapp/Bus.java
+++ app/src/main/java/com/kchen52/yetanothertranslinkapp/Bus.java
@@ -3,9 +3,6 @@ package com.kchen52.yetanothertranslinkapp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by Kevin on 2016-09-09.
- */
 public class Bus {
     private String destination;
     private int vehicleNumber;
@@ -21,10 +18,19 @@ public class Bus {
 
     // Basically the same algorithm that the servlet uses
     public void init(String rawInfo) {
-        destination = setValue("Destination", rawInfo);
-        vehicleNumber = Integer.parseInt(setValue("VehicleNo", rawInfo));
-        longitude = Double.parseDouble(setValue("Longitude", rawInfo));
-        latitude = Double.parseDouble(setValue("Latitude", rawInfo));
+        try {
+            destination = setValue("Destination", rawInfo);
+            vehicleNumber = Integer.parseInt(setValue("VehicleNo", rawInfo));
+            longitude = Double.parseDouble(setValue("Longitude", rawInfo));
+            latitude = Double.parseDouble(setValue("Latitude", rawInfo));
+        } catch (IllegalStateException e) {
+            // This happens if the init string is in an invalid format. Set all members to their
+            // default values.
+            destination = "null";
+            vehicleNumber = -1;
+            longitude = 0.0;
+            latitude = 0.0;
+        }
     }
 
     private String setValue(String valueToSet, String input) {
@@ -35,15 +41,24 @@ public class Bus {
     }
 
     public void init(String busDestination, String input) {
-        //Note: input is in the form 8122:-122.842117,-122.842117
-        // Where the long/lat value is Lat, and second is Long
-        String[] initialSplit = input.split(":");
-        vehicleNumber = Integer.parseInt(initialSplit[0]);
-        destination = busDestination;
+        try {
+            //Note: input is in the form 8122:-122.842117,-122.842117
+            // Where the long/lat value is Lat, and second is Long
+            String[] initialSplit = input.split(":");
+            vehicleNumber = Integer.parseInt(initialSplit[0]);
+            destination = busDestination;
 
-        String longAndLat = initialSplit[1];
-        latitude = Double.parseDouble(longAndLat.split(",")[0]);
-        longitude = Double.parseDouble(longAndLat.split(",")[1]);
+            String longAndLat = initialSplit[1];
+            latitude = Double.parseDouble(longAndLat.split(",")[0]);
+            longitude = Double.parseDouble(longAndLat.split(",")[1]);
+        } catch (IllegalStateException | NumberFormatException e) {
+            // This happens if the init strings is in an invalid format. Set all members to their
+            // default values.
+            destination = "null";
+            vehicleNumber = -1;
+            longitude = 0.0;
+            latitude = 0.0;
+        }
     }
 
     public String getDestination() {
