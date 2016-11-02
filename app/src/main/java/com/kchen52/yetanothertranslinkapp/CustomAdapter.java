759eba74063d955ccb1d12a297de38fe3775bf28
Kevin Chen <k.chen52@gmail.com>
2016-11-02 14:24:28 -0700

    Clicking on text in bus list now toggles its corresponding checkbox

--- app/src/main/java/com/kchen52/yetanothertranslinkapp/CustomAdapter.java
+++ app/src/main/java/com/kchen52/yetanothertranslinkapp/CustomAdapter.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,6 +39,57 @@ public class CustomAdapter extends ArrayAdapter<Model> {
                 String savedBuses = sharedPref.getString(context.getString(R.string.saved_buses_requested), context.getString(R.string.saved_buses_requested_default));
                 String checkedBusText = (String) name.getText();
                 String checkedBusNumber = checkedBusText.split(" - ")[0];
+                if (cb.isChecked()) {
+                    // Check if the bus exists in the list of buses, and add it if not
+                    if (!busAlreadySaved(checkedBusNumber, savedBuses)) {
+                        // Append the current bus to the end of savedBuses, and save it to the sharedpref
+                        String busNumber = ((String)name.getText()).split(" - ")[0];
+                        if (savedBuses.equals("")) {
+                            savedBuses = busNumber;
+                        } else {
+                            savedBuses = savedBuses + ", " + busNumber;
+                        }
+
+                        // And save changes into the sharedpreferences
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putString(context.getString(R.string.saved_buses_requested), savedBuses);
+                        editor.commit();
+                    }
+                } else {
+                    // Check if the bus exists in the list of buses, and remove it if so
+                    if (busAlreadySaved(checkedBusNumber, savedBuses)) {
+                        StringBuilder builder = new StringBuilder();
+
+                        // Not the best way to do this, but we need a comma after each element, except the first
+                        int index = 0;
+                        for (String bus : savedBuses.split(", ")) {
+                            if (!bus.equals(checkedBusNumber)) {
+                                if (index > 0) {
+                                    builder.append(", ");
+                                }
+                                builder.append(bus);
+                                index++;
+                            }
+                        }
+                        String newFormattedBusList = builder.toString();
+
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putString(context.getString(R.string.saved_buses_requested), newFormattedBusList);
+                        editor.commit();
+                    }
+                }
+            }
+        });
+
+        name.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+
+                String savedBuses = sharedPref.getString(context.getString(R.string.saved_buses_requested), context.getString(R.string.saved_buses_requested_default));
+                String checkedBusText = (String) name.getText();
+                String checkedBusNumber = checkedBusText.split(" - ")[0];
+                cb.toggle();
+
                 if (cb.isChecked()) {
                     // Check if the bus exists in the list of buses, and add it if not
                     if (!busAlreadySaved(checkedBusNumber, savedBuses)) {
