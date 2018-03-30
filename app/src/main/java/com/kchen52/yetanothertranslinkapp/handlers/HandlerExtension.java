6566e0ee238c8a360ee4aeba9a8813f44ddf4225
Kevin Chen <kevin.chen@eventbase.com>
2018-03-30 13:12:16 +1000

    Actually add moved handlers

---
+++ app/src/main/java/com/kchen52/yetanothertranslinkapp/handlers/HandlerExtension.java
@@ -0,0 +1,26 @@
+package com.kchen52.yetanothertranslinkapp.handlers;
+
+import android.os.Handler;
+import android.os.Message;
+
+import com.kchen52.yetanothertranslinkapp.MapsActivity;
+
+import java.lang.ref.WeakReference;
+
+// For handling responses from the requestHandler
+public class HandlerExtension extends Handler {
+    private final String TAG = "HandlerExtension";
+    private final WeakReference<MapsActivity> currentActivity;
+
+    public HandlerExtension(MapsActivity activity) {
+        currentActivity = new WeakReference<>(activity);
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+        MapsActivity mapsActivity = currentActivity.get();
+        if (mapsActivity != null) {
+            mapsActivity.updateDisplayedTimeAndDrawBuses(msg);
+        }
+    }
+}
